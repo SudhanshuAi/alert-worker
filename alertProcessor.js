e2cb9016c5ffca5fd8fbebbaa6f18c7602c437e2
Meag26 <sudhanshu26uv@gmail.com>
2025-08-29 13:07:22 +0530

    update

--- alertProcessor.js
+++ alertProcessor.js
@@ -5,8 +5,8 @@ const { Worker } = require('bullmq');
 const { createClient } = require('@supabase/supabase-js');
 const { Pool } = require('pg');
 const { Redis } = require('ioredis');
-// const { sendMessageWithBotToken } = require('./slackWebApiNotifier');
-const { sendNotificationToSlack } = require('./sendSlackNotification');
+const { sendMessageWithBotToken } = require('./slackWebApiNotifier');
+// const { sendNotificationToSlack } = require('./sendSlackNotification');
 
 // --- Client Initializations ---
 const redisConnection = new Redis(process.env.REDIS_URL, { maxRetriesPerRequest: null });
@@ -129,7 +129,7 @@ const alertJobProcessor = async (job) => {
                 logDetails = { value: newData.value, threshold: threshold, operator: operator };
 
                 if (triggered) {
-                    await sendNotificationToSlack({
+                    await sendMessageWithBotToken({
                         name: ruleName, id: currentRule.id, currentValue: newData.value,
                         operator: operator, threshold: threshold,
                     });
